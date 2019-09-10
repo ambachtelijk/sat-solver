754a992ce55ff50bee7f2c9e0fe37f8b000a013f
Rick van Ravensberg <ehmplankje@gmail.com>
2019-09-10 20:57:40 +0200

    Fixed DIMACS files to contain valid syntax

--- sat-solver.py
+++ sat-solver.py
@@ -1,13 +1,13 @@
 import sys
-
-from sympy.logic.utilities.dimacs import load_file
+import mxklabs.dimacs
 
 
 def main():
-    dimacs_filename = sys.argv[1]
-    dimacs = load_file(dimacs_filename)
-    print("hello, world")
-    print(dimacs)
+    dimacs_rules_filename = sys.argv[1]
+    dimacs_sudoku_filename = sys.argv[2]
+    dimacs_rules = mxklabs.dimacs.read(dimacs_rules_filename)
+    dimacs_sudoku = mxklabs.dimacs.read(dimacs_sudoku_filename)
+    foo = dimacs_sudoku
 
 
 main()
