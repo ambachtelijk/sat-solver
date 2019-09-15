23695d32c6caaa873d1eb2bd94571553a95c9cf0
Rick <ehmplankje@gmail.com>
2019-09-15 09:09:30 +0200

    Full David Putman implementation with DLCS split

--- SAT.py
+++ SAT.py
@@ -1,3 +1,4 @@
+import copy
 import sys
 import timeit
 from typing import Set, Dict, List
@@ -5,6 +6,9 @@ from typing import Set, Dict, List
 import mxklabs.dimacs
 
 start = timeit.default_timer()
+NEG = 'negative'
+POS = 'positive'
+FRQ = 'frequency'
 
 
 def main():
@@ -17,21 +21,25 @@ def main():
     # early on anyway, so there is no need to run the algorithm twice.
     clauses: List[Set[int]] = [*map(lambda clause: {*clause}, solved.clauses + problem.clauses)]
     resolved: Dict[int, bool] = {}
-    unresolved: Set[int] = extract_vars(clauses)
     unprocessed: Set[int] = set()
-    print("{} variables extracted. {} clauses".format(len(unresolved), len(clauses)))
-    result: bool = dp(clauses, resolved, unresolved, unprocessed, len(clauses))
+
+    print("{} clauses".format(len(clauses)))
+
+    result: bool = dp(clauses, resolved, unprocessed, len(clauses), list())
 
     if result:
-        print("Solution found. {} variables resolved, {} variables unresolved."
-              .format(len(resolved), len(unresolved)))
+        print("Solution found. {} variables resolved."
+              .format(len(resolved)))
     else:
-        print("No solution found. {} variables resolved, {} variables unresolved. {} clauses remaining."
-              .format(len(resolved), len(unresolved), len(clauses)))
+        print("No solution found. {} variables resolved."
+              .format(len(resolved)))
 
 
-def dp(clauses: List[Set[int]], resolved: Dict[int, bool], unresolved: Set[int], unprocessed: Set[int],
-       num_clauses: int):
+def dp(clauses: List[Set[int]],
+       resolved: Dict[int, bool],
+       unprocessed: Set[int],
+       prev_num_clauses: int,
+       depth):
     # Remove all clauses that contain a resolved variable.
     for literal in unprocessed:
         polar_literal = literal * -1
@@ -50,7 +58,7 @@ def dp(clauses: List[Set[int]], resolved: Dict[int, bool], unresolved: Set[int],
         clause_length: int = len(clause)
         # Test for empty clauses.
         # @todo fix DIMACS parser that it won't drop empty clauses
-        if len(clause) == 0:
+        if clause_length == 0:
             return False
 
         # Test for tautologies.
@@ -60,16 +68,16 @@ def dp(clauses: List[Set[int]], resolved: Dict[int, bool], unresolved: Set[int],
 
         # Test for unit clauses.
         if clause_length == 1:
+            clauses.remove(clause)
             literal: int = clause.pop()
             variable: int = abs(literal)
             polarity: bool = variable == literal
             if variable not in resolved:
                 resolved[variable] = polarity
-                unresolved.remove(variable)
                 unprocessed.add(literal)
+            # We encountered a contradiction.
             elif resolved[variable] != polarity:
                 return False
-            clauses.remove(clause)
             continue
 
         # Test for pure literals.
@@ -78,7 +86,7 @@ def dp(clauses: List[Set[int]], resolved: Dict[int, bool], unresolved: Set[int],
             if variable in rejected_variables:
                 continue
 
-            polar_literal = literal * 1
+            polar_literal = literal * -1
             if polar_literal in pure_literals:
                 pure_literals.pop(polar_literal)
                 rejected_variables.add(variable)
@@ -88,30 +96,48 @@ def dp(clauses: List[Set[int]], resolved: Dict[int, bool], unresolved: Set[int],
     for variable in pure_literals:
         if variable not in resolved:
             resolved[variable] = pure_literals[variable]
-            unresolved.remove(variable)
             unprocessed.add(variable if pure_literals[variable] == abs(pure_literals[variable]) else variable * -1)
         elif resolved[variable] != pure_literals[variable]:
             return False
 
-    # len_clauses = len(clauses)
-    # len_resolved = len(resolved)
-    # len_unresolved = len(unresolved)
-    # Test for an empty set of clauses.
-    if len(pure_literals) == 0:
-        if len(clauses) == 0:
+    num_clauses = len(clauses)
+    if len(unprocessed) == 0:
+        # Test for an empty set of clauses.
+        if num_clauses == 0:
             return True
-        elif len(clauses) == num_clauses:
-            for variable in resolved:
-                if resolved[variable]:
-                    print(variable)
+        elif num_clauses == prev_num_clauses:
+            unresolved: Dict[str, Dict[int, int]] = extract_vars(clauses)
+            sort = sorted(unresolved[FRQ].items(), key=lambda x: x[1], reverse=True)
+            for direction in [True]:
+                for variable, frequency in sort:
+                    # polarity = direction if unresolved[POS][variable] < unresolved[NEG][variable] else not direction
+                    polarity = direction
+                    literal = variable if polarity else variable * -1
+
+                    print("Stack {} Trying {}".format(depth, literal))
+                    if dp(copy.deepcopy(clauses), {variable: polarity, **resolved}, {literal}, num_clauses, [*depth, literal]):
+                        return True
             return False
-    return dp(clauses, resolved, unresolved, unprocessed, len(clauses))
+
+    return dp(clauses, resolved, unprocessed, num_clauses, depth)
 
 
 def extract_vars(clauses: List[Set[int]]):
-    result: Set[int] = set()
+    result: Dict[str, Dict[int, int]] = {
+        NEG: {},
+        POS: {},
+        FRQ: {},
+    }
     for clause in clauses:
-        result = result.union(extract_clause_vars(clause))
+        for literal in clause:
+            variable = abs(literal)
+            polarity = literal == variable
+            if variable not in result[FRQ]:
+                result[NEG][variable] = 0
+                result[POS][variable] = 0
+                result[FRQ][variable] = 0
+            result[POS if polarity else NEG][variable] += 1
+            result[FRQ][variable] += 1
     return result
 
 
