648fc527157f6bc638306b4f3f3cd30f2c55b6f5
Ruben <ruub_azfan@hotmail.com>
2019-09-19 12:11:35 +0200

    A python file for writing output file in cnf, .close() added. Comments added

--- SAT.py
+++ SAT.py
@@ -201,6 +201,23 @@ def split_dlcs(clauses: List[Set[int]],
     return False
 
 
+# Writes an output file in DIMACS format
+def outputFile(output_directory: str,
+               filename_out: str,
+               resolved_dict: Dict[int, bool]):
+    file_out: str = os.path.join(output_directory, filename_out)
+    output_file: IO = open(file_out, 'w')
+    output_file.write("p cnf {} {}\n".format(str(max(resolved_dict)), str(len(resolved_dict))))
+
+    # Writes every element of dict in new line of output file
+    for clause_out in resolved_dict:
+        if not resolved_dict[clause_out]:
+            clause_out *= -1
+        output_file.write("{} 0\n".format(str(clause_out)))
+
+    output_file.close()
+
+
 def extract_clause_vars(clause: Set[int]) -> Set[int]:
     # Get the variable name from all literals in the clause by removing any hyphens.
     return {*map(lambda literal: abs(literal), clause)}
