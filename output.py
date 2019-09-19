01446332e6d2b21ee575e8e3af7b9d5624456f13
Ruben <ruub_azfan@hotmail.com>
2019-09-19 12:04:44 +0200

    A python file for writing output file in cnf

---
+++ output.py
@@ -0,0 +1,25 @@
+from typing import IO, Dict, List
+import os
+
+
+# Uncomment to test code
+# test_dict = {111: True, 112: True, 113: False, 567: True, 783: False}
+# path = 'C:\Studie Projecten\SAT Solver\output'
+# file_out: str = os.path.join(path, 'test_out.txt')
+
+
+# Writes an output file in DIMACS format
+def outputFile(output_directory: str, filename_out: str,
+               resolved_dict: Dict[int, bool]):  # moet nog ' filename_out: str,' bij
+    file_out: str = os.path.join(output_directory, filename_out)
+    output_file: IO = open(file_out, 'w')
+    output_file.write("p cnf {} {}\n".format(str(max(resolved_dict)), str(len(resolved_dict))))
+
+    # Creates a list of all truth statements for output file
+    for clause_out in resolved_dict:
+        if not resolved_dict[clause_out]:
+            clause_out *= -1
+        output_file.write("{} 0\n".format(str(clause_out)))
+
+# Uncomment to test code
+# outputFile(path, 'test_out.txt', test_dict)
