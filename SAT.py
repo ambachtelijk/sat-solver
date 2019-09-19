8f3d720ae07bd6e8dd732c7b2749870885a699af
Rick <ehmplankje@gmail.com>
2019-09-19 21:15:25 +0200

    Fixed missing imports from dimacs writer

--- SAT.py
+++ SAT.py
@@ -1,8 +1,9 @@
 import copy
 import math
+import os
 import sys
 import time
-from typing import Set, Dict, List, Tuple
+from typing import Set, Dict, List, Tuple, IO
 
 import mxklabs.dimacs
 from mxklabs.dimacs import Dimacs
@@ -218,17 +219,20 @@ def split_dlcs(clauses: List[Set[int]],
     return False, resolved
 
 
+# Uncomment to test code
+# test_dict = {111: True, 112: True, 113: False, 567: True, 783: False}
+# path = 'C:\Studie Projecten\SAT Solver\output'
 # Writes an output file in DIMACS format
-def outputFile(output_directory: str,
-               filename_out: str,
-               resolved_dict: Dict[int, bool]):
+def write_dimacs(output_directory: str,
+                 filename_out: str,
+                 solution: Dict[int, bool]):
     file_out: str = os.path.join(output_directory, filename_out)
     output_file: IO = open(file_out, 'w')
-    output_file.write("p cnf {} {}\n".format(str(max(resolved_dict)), str(len(resolved_dict))))
+    output_file.write("p cnf {} {}\n".format(str(max(solution)), str(len(solution))))
 
     # Writes every element of dict in new line of output file
-    for clause_out in resolved_dict:
-        if not resolved_dict[clause_out]:
+    for clause_out in solution:
+        if not solution[clause_out]:
             clause_out *= -1
         output_file.write("{} 0\n".format(str(clause_out)))
 
