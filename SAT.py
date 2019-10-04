e66ab86f610170d148f7bb0a0f8dcc90db782fab
Rick <ehmplankje@gmail.com>
2019-10-04 21:10:59 +0200

    DLCS and DLIS heuristics

--- SAT.py
+++ SAT.py
@@ -11,10 +11,9 @@ import mxklabs.dimacs
 from mxklabs.dimacs import Dimacs
 
 start: float = time.perf_counter()
-split = sys.argv[3] if len(sys.argv) >= 4 else 'dlcs'
 
-PREPROCESS = False
-PURE_LITERALS = True
+PREPROCESS = True
+PURE_LITERALS = False
 
 
 # noinspection PyBroadException
@@ -40,7 +39,19 @@ def main(problem_filename: str, solvable_filename: str) -> None:
                     break
 
         if success:
-            success, solution, clauses, conflict = dp(clauses, solution, unprocessed)
+            if split == "dlcs_reversed" or split == "dlis_reversed" or split == 'fifo_reversed':
+                order = [False, True]
+
+            elif split == "dlcs_positive_only" or split == "dlis_positive_only" or split == 'fifo_positive_only':
+                order = [True]
+
+            elif split == "dlcs_negative_only" or split == "dlis_negative_only" or split == 'fifo_negative_only':
+                order = [False]
+
+            else:
+                order = [True, False]
+
+            success, solution, clauses, conflict = dp(clauses, solution, unprocessed, order=order)
 
         if success:
             positive_vars: List[int] = [*sorted(filter(lambda _variable: solution[_variable], solution))]
@@ -55,10 +66,8 @@ def main(problem_filename: str, solvable_filename: str) -> None:
         else:
             print("No solution found.")
 
-    except Exception:
-        tb = traceback.format_exc()
-        print(tb)
-        exit()
+    except Exception as e:
+        print(e)
 
     print("DP calls: {},\nLiteral iterations: {},\nLiteral searches: {}\n".format(
         globals()['dp_call'],
@@ -72,11 +81,10 @@ def main(problem_filename: str, solvable_filename: str) -> None:
 def dp(clauses: List[Set[int]],
        solution: Dict[int, Optional[bool]],
        unprocessed: Set[int],
-       call_split: bool = True,
        **extra_vars) -> Tuple[bool, Dict[int, bool], List[Set[int]], Optional[int]]:
     count_dp()
-    # if time.process_time() - globals()['start'] > 30:
-    #     raise Exception("Time of 30 seconds exceeded.")
+    if globals()['dp_call'] > 10000:
+        raise Exception("Unsolvable, limit of 10000 DP calls exceeded.")
 
     # Test for pure literals.
     pure_literals: Dict[int, int] = {}
@@ -88,9 +96,9 @@ def dp(clauses: List[Set[int]],
         removed: bool = False
         # Remove all clauses that contain a resolved variable.
         for literal in unprocessed:
-            count_literal_iteration()
+            # count_literal_iteration()
             # Remove the whole clause if it contains the resolved literal (therefore the clause resolves to True).
-            count_literal_search()
+            # count_literal_search()
             if literal in clause:
                 clauses.remove(clause)
                 removed = True
@@ -98,7 +106,7 @@ def dp(clauses: List[Set[int]],
 
             # Remove the opposite literal from the clause, because the instance will resolve to False.
             polar_literal: int = literal * -1
-            count_literal_search()
+            # count_literal_search()
             if polar_literal in clause:
                 clause.remove(polar_literal)
                 clauses[clauses.index(clause)] = clause
@@ -129,15 +137,15 @@ def dp(clauses: List[Set[int]],
         if PURE_LITERALS:
             # Keep track of all pure literals that we may encounter.
             for literal in clause:
-                count_literal_iteration()
+                # count_literal_iteration()
                 variable: int = abs(literal)
                 # Keep track if this literal was marked as not pure, so we don't have to probe it again.
-                count_literal_search()
+                # count_literal_search()
                 if variable in rejected_variables:
                     continue
                 polar_literal: int = literal * -1
                 # The current literal is not pure, because its opposite value exists.
-                count_literal_search()
+                # count_literal_search()
                 if polar_literal in pure_literals:
                     pure_literals.pop(polar_literal)
                     rejected_variables.add(variable)
@@ -146,7 +154,7 @@ def dp(clauses: List[Set[int]],
 
     if PURE_LITERALS:
         # Test for pure literals.
-        count_literal_search()
+        # count_literal_search()
         for variable in pure_literals:
             if not resolve(pure_literals[variable], solution, new_unprocessed):
                 # We encountered a contradiction.
@@ -154,72 +162,138 @@ def dp(clauses: List[Set[int]],
 
     # There is still something to process, rerun with the current context.
     if len(new_unprocessed) != 0:
-        return dp(clauses, solution, new_unprocessed, call_split, **extra_vars)
+        return dp(clauses, solution, new_unprocessed, **extra_vars)
 
     # Test if there are still unresolved clauses, which means that we're not done yet with parsing.
-    elif call_split and len(clauses) != 0:
+    elif len(clauses) != 0:
         # Perform a Split operation.
         return {
-            'fcfs': split_fcfs,
-            # 'dlcs': split_dlcs,
+            'fifo': split_fifo,
+            'fifo_reversed': split_fifo,
+            'fifo_positive_only': split_fifo,
+            'fifo_negative_only': split_fifo,
+            'dlcs': split_dlcs,
+            'dlcs_reversed': split_dlcs,
+            'dlcs_positive_only': split_dlcs,
+            'dlcs_negative_only': split_dlcs,
+            'dlis': split_dlis,
+            'dlis_reversed': split_dlis,
+            'dlis_positive_only': split_dlis,
+            'dlis_negative_only': split_dlis,
         }[split](clauses, solution, **extra_vars)
 
     # Found a solution!
     return True, solution, clauses, None
 
 
-# # Dynamic Largest Combined Sum split.
-# def split_dlcs(clauses: List[Set[int]],
-#                resolved: Dict[int, bool],
-#                backtrace: List[int] = None,
-#                stack: List[int] = None) -> Union[bool, Dict[int, bool]]:
-#     if backtrace is None:
-#         backtrace = []
-#     if stack is None:
-#         stack = []
-#
-#     # Get all variables from the clauses.
-#     unresolved_neg: Dict[int, int] = {}
-#     unresolved_pos: Dict[int, int] = {}
-#     unresolved_frq: Dict[int, int] = {}
-#     for clause in clauses:
-#         for literal in clause:
-#             if literal in backtrace:
-#                 continue
-#             variable: int = abs(literal)
-#             polarity: bool = literal == variable
-#             if variable not in unresolved_frq:
-#                 unresolved_neg[variable] = 0
-#                 unresolved_pos[variable] = 0
-#                 unresolved_frq[variable] = 0
-#             (unresolved_pos if polarity else unresolved_neg)[variable] += 1
-#             unresolved_frq[variable] += 1
-#
-#     # We will resolve the open variables in order of total frequency (both negative and positive literals combined).
-#     sort: List[Tuple[int, int]] = [*sorted(unresolved_frq.items(), key=lambda x: x[1], reverse=True)]
-#     # for variable in sorted(unresolved_frq):
-#     for variable, frq in sort:
-#         for direction in [True, False]:
-#             polarity: bool = direction if unresolved_pos[variable] > unresolved_neg[variable] else not direction
-#             literal: int = variable if polarity else variable * -1
-#             # Re-run the algorithm with a new literal value.
-#             result: Union[bool, Dict[int, bool]] = dp(
-#                 # Do a deep copy, because otherwise it gets stuck with clause simplifications from previous attempts.
-#                 copy.deepcopy(clauses),
-#                 {variable: polarity, **resolved},
-#                 {literal},
-#                 backtrace=[*backtrace],
-#                 stack=[*stack, literal]
-#             )
-#             if result:
-#                 return result
-#             # Append the current literal to the backtrace, so that it won't get scanned again.
-#             backtrace.append(literal)
-#     return False
-
-# First-come first-serve split
-def split_fcfs(clauses: List[Set[int]],
+# # Dynamic Largest Individual Sum split.
+def split_dlis(clauses: List[Set[int]],
                solution: Dict[int, bool],
+               order: List[bool],
+               backtrace: List[int] = None,
+               stack: List[int] = None):
+    if backtrace is None:
+        backtrace = set()
+    if stack is None:
+        stack = []
+
+    # Get all variables from the clauses.
+    unresolved: Dict[int, int] = {}
+    for clause in clauses:
+        for literal in clause:
+            variable = abs(literal)
+            polarity = variable == literal
+            if solution[variable] is not None or literal in backtrace or literal * -1 in backtrace:
+                continue
+            if literal not in unresolved:
+                unresolved[literal] = 0
+            if literal * -1 not in unresolved:
+                # This should never happen, because it indicates the existence of a pure literal.
+                unresolved[literal * -1] = 0
+            unresolved[literal] += 1
+
+    # We will resolve the open variables in order of total frequency (both negative and positive literals combined).
+    sort: List[Tuple[int, int]] = [*sorted(unresolved.items(), key=lambda x: x[1], reverse=True)]
+    # for variable in sorted(unresolved_frq):
+    for direction in order:
+        for literal, frq in sort:
+            variable: int = abs(literal)
+            polarity: bool = direction if unresolved[literal] > unresolved[literal * -1] else not direction
+            # Append the current literal to the backtrace, so that it won't get scanned again.
+            backtrace.add(literal)
+            # Re-run the algorithm with a new literal value.
+            success, _solution, _clauses, conflict = dp(
+                # Do a deep copy, because otherwise it gets stuck with clause simplifications from previous attempts.
+                copy.deepcopy(clauses),
+                {**solution, variable: polarity},
+                {literal},
+                backtrace={*backtrace},
+                stack=[*stack, literal],
+                order=order
+            )
+            if success:
+                return success, _solution, _clauses, conflict
+
+    return False, solution, clauses, None
+
+
+# # Dynamic Largest Combined Sum split.
+def split_dlcs(clauses: List[Set[int]],
+               solution: Dict[int, bool],
+               order: List[bool],
+               backtrace: List[int] = None,
+               stack: List[int] = None):
+    if backtrace is None:
+        backtrace = set()
+    if stack is None:
+        stack = []
+
+    # Get all variables from the clauses.
+    unresolved_neg: Dict[int, int] = {}
+    unresolved_pos: Dict[int, int] = {}
+    unresolved_frq: Dict[int, int] = {}
+    for clause in clauses:
+        for literal in clause:
+            variable = abs(literal)
+            polarity = variable == literal
+            if solution[variable] is not None or literal in backtrace or literal * -1 in backtrace:
+                continue
+            if variable not in unresolved_frq:
+                unresolved_neg[variable] = 0
+                unresolved_pos[variable] = 0
+                unresolved_frq[variable] = 0
+            (unresolved_pos if polarity else unresolved_neg)[variable] += 1
+            unresolved_frq[variable] += 1
+
+    # We will resolve the open variables in order of total frequency (both negative and positive literals combined).
+    sort: List[Tuple[int, int]] = [*sorted(unresolved_frq.items(), key=lambda x: x[1], reverse=True)]
+    # for variable in sorted(unresolved_frq):
+    for direction in order:
+        for variable, frq in sort:
+            polarity: bool = direction if unresolved_pos[variable] > unresolved_neg[variable] else not direction
+            literal: int = variable if polarity else variable * -1
+            # Append the current literal to the backtrace, so that it won't get scanned again.
+            backtrace.add(literal)
+            # Re-run the algorithm with a new literal value.
+            success, _solution, _clauses, conflict = dp(
+                # Do a deep copy, because otherwise it gets stuck with clause simplifications from previous attempts.
+                copy.deepcopy(clauses),
+                {**solution, variable: polarity},
+                {literal},
+                backtrace={*backtrace},
+                stack=[*stack, literal],
+                order=order
+            )
+            if success:
+                return success, _solution, _clauses, conflict
+
+    return False, solution, clauses, None
+
+
+# First in first out split
+def split_fifo(clauses: List[Set[int]],
+               solution: Dict[int, bool],
+               order: List[bool],
                backtrace: Set[int] = None,
                stack: List[int] = None) -> Tuple[bool, Dict[int, bool], List[Set[int]], Optional[int]]:
     if backtrace is None:
@@ -228,9 +302,9 @@ def split_fcfs(clauses: List[Set[int]],
         stack = []
 
     # We will resolve the open variables in order of total frequency (both negative and positive literals combined).
-    print(stack)
+    # print(stack)
 
-    for direction in [True, False]:
+    for direction in order:
         for variable, polarity in solution.items():
             if polarity is not None or variable in backtrace or variable * -1 in backtrace:
                 continue
@@ -247,7 +321,8 @@ def split_fcfs(clauses: List[Set[int]],
                 {**solution, variable: direction},
                 {literal},
                 backtrace={*backtrace},
-                stack=[*stack, literal]
+                stack=[*stack, literal],
+                order=order
             )
             if success:
                 return success, _resolved, _clauses, None
@@ -289,7 +364,7 @@ def resolve(_literal: int, _resolved: Dict[int, Optional[bool]], _unprocessed: S
 
 def extract_clause_vars(clause: Set[int]) -> Set[int]:
     def cb(literal):
-        count_literal_iteration()
+        # count_literal_iteration()
         return abs(literal)
 
     # Get the variable name from all literals in the clause by removing any hyphens.
@@ -297,7 +372,7 @@ def extract_clause_vars(clause: Set[int]) -> Set[int]:
 
 
 def remove_literal(clause: Set[int], literal: int) -> Set[int]:
-    count_literal_search()
+    # count_literal_search()
     if literal in clause:
         clause.remove(literal)
     return clause
@@ -341,15 +416,25 @@ def count_literal_search():
     globals()['literal_search'] += 1
 
 
-with open('output/stats.csv', mode='w') as stats_file:
+offset = int(sys.argv[3]) if len(sys.argv) >= 4 else 0
+splits = [sys.argv[4]] if len(sys.argv) >= 5 else [
+    'fifo', 'fifo_reversed', 'fifo_negative_only', 'fifo_positive_only',
+    'dlcs', 'dlcs_reversed', 'dlcs_negative_only', 'dlcs_positive_only',
+    'dlis', 'dlis_reversed', 'dlis_negative_only', 'dlis_positive_only',
+]
+
+with open("output/stats-{}.csv".format(offset), mode='w', newline='') as stats_file:
     stats = csv.writer(stats_file)
 
-    for i in range(1000):
-        try:
+    stats.writerow(['sudoku', 'split', 'dp_call', 'literal_iteration', 'literal_search'])
+    for i in range(offset, offset + 100):
+        for split in splits:
+            print("Split: " + split)
             globals()['literal_iteration'] = 0
             globals()['literal_search'] = 0
             globals()['dp_call'] = 0
-            main(sys.argv[1], sys.argv[2].format(str(i).rjust(4, '0'), 'dc', stats))
-            stats.writerow([i, globals()['dp_call'], globals()['literal_iteration'], globals()['literal_search']])
-        except Exception as e:
-            print("Exception: ", str(e), "\n")
+            main(sys.argv[1], sys.argv[2].format(str(i).rjust(4, '0'), 'dc'))
+            stats.writerow(
+                [i, split, globals()['dp_call'], globals()['literal_iteration'], globals()['literal_search']])
+
+        stats_file.flush()
